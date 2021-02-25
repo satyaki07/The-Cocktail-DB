f5eea9c1e830062513aa0223f43582f2dcc54d96
Satyaki Bose <satyakibose98@gmail.com>
2021-02-25 15:41:42 +0530

    Setup pages

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        adipisci soluta, eos iusto dolorem dolorum similique voluptatem, aliquid
+        voluptate reprehenderit nisi tempore fuga repellat ad rem, rerum velit
+        atque qui illo? Suscipit incidunt ea voluptates qui aliquam voluptatum
+        quae. Quos.
+      </p>
+    </section>
+  );
+};
+
+export default About;
