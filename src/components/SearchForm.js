90c66e36c60f3c83fe076aa765b792940fd9defe
Satyaki Bose <satyakibose98@gmail.com>
2021-02-28 12:26:02 +0530

    useCallback

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "../context";
 import Cocktail from "./Cocktail";
 
@@ -6,15 +6,24 @@ const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = useRef("");
 
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">seach your favourite cocktail</label>
           <input
+            autoComplete="off"
             type="text"
             id="name"
             ref={searchValue}
